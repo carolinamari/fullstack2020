88b4a314985f9af9e2a705660a2af5f34b892cf3
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-11-24 16:27:04 -0300

    minor fix

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,44 +1,44 @@
 import React from 'react'
 
 const Header = ({ course }) => {
-    return (
-      <h2>{course.name}</h2>
-    )
-  }
-  
-  const Total = ({ course }) => {
-    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-    return(
-      <p><b>Total of {total} exercises</b></p>
-    ) 
-  }
-  
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>    
-    )
-  }
-  
-  const Content = ({ course }) => {
-    return (
-      <div>
-        {course.parts.map(part => 
-          <Part key={part.id} part={part} />
-        )}
-      </div>
-    )
-  }
-  
-  const Course = ({ course }) => {
-    return (
-      <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-      </div>
-    )
-  }
+  return (
+    <h2>{course.name}</h2>
+  )
+}
 
-  export default Course
+const Total = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return(
+    <p><b>Total of {total} exercises</b></p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Course
